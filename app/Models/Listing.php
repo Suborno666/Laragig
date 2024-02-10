90c43cb10a3c394b91e17b3b580852afddf8220b
Suborno22 <dsuborno0@gmail.com>
2024-02-10 16:32:27 +0530

    Fresh Upload

--- app/Models/Listing.php
+++ app/Models/Listing.php
@@ -1,30 +1,11 @@
 <?php
- namespace App\Models;
- 
- class Listing{
-    public static function all(){
-        return [
-            [
-                'id'=>1,
-                'title'=>'Listing One',
-                'description'=>"Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores molestias a quibusdam consequatur! Minus eos hic
-                voluptatem, repudiandae aut aliquam deleniti veniam harum officiis aperiam alias voluptate temporibus tenetur rem."
-            ],
-            [
-                'id'=>2,
-                'title'=>'Listing Two',
-                'description'=>"Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores molestias a quibusdam consequatur! Minus eos hic
-                voluptatem, repudiandae aut aliquam deleniti veniam harum officiis aperiam alias voluptate temporibus tenetur rem."
-            ]
-        ];
-    }
 
-    public static function find($id){
-        $listings = self::all();
-        foreach($listings as $listing){
-            if($listing['id'] == $id){
-                return $listing;
-            }
-        }
-    }
- }
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Listing extends Model
+{
+    use HasFactory;
+}
