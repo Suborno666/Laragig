552f63d5e9a1b67a2c20cf28526a918ef297e019
Suborno22 <dsuborno0@gmail.com>
2024-02-14 00:57:30 +0530

    added form

--- routes/web.php
+++ routes/web.php
@@ -15,8 +15,25 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+// Common Resources Index:
+//  index - Shows all Listing
+//  show - Shows single Listing
+//  create - Show form to create new Listing
+//  store - Store new listing
+//  edit - Show form to edit listing
+//  update - update listing
+//  destroy - Delete listing 
+ 
+
 // All listing
 Route::get('/',[ListingController::class,'index']);
+
+// Show create form
+
+Route::get('/listings/create',[ListingController::class,'create']);
+
+// Store data from form
+Route::post('/listings',[ListingController::class,'store']);
 
 //Single Listing
 Route::get('/listings/{listing}',[ListingController::class,'show']);
